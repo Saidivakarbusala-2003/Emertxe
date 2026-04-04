793f2e42ae99d1e1256c545e0ae91479c8117beb
Sai Divakar <saidivakarbusala@gmail.com>
2026-04-04 12:35:53 +0530

    Delete Addressbook/AddressBook_project1 directory

--- Addressbook/AddressBook_project1/file.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_H
-#define FILE_H
-#include "contact.h"
-
-void saveContactsToFile(AddressBook *addressBook);// function declare for save contact to file
-
-void loadContactsFromFile(AddressBook *addressBook);//funtion declare for loadcontactsfromfile
-
-#endif
